f74b6eda77ff67c9813c7d85d775874101970e70
Clayton Gorman <claygorman@gmail.com>
2021-11-06 13:32:45 -0700

    Adding sonos cask

---
+++ Casks/sonos.rb
@@ -0,0 +1,20 @@
+cask 'sonos' do
+  version '13.3.2,65.1.22050'
+  sha256 '2a0f2aebe3e10c52c3905d0f6eb7a060903f486ec418bc2ba895d7feef7e46e0'
+
+  url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.before_comma.no_dots}.dmg"
+  name 'Sonos'
+  desc 'Control your Sonos system'
+  homepage 'https://www.sonos.com/'
+
+  livecheck do
+    url 'https://www.sonos.com/en/redir/controller_software_mac2'
+    strategy :extract_plist
+  end
+
+  auto_updates true
+
+  app 'Sonos.app'
+
+  zap trash: '~/Library/Application Support/SonosV2'
+end
